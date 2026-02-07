87969946b11c490fc47bbd43b1cfe54e96fce2e6
Maru Karthik Reddy <karthikreddy.maru@gmail.com>
2026-02-07 10:48:57 +0530

    feat: add meta-data in meals screen

--- lib/screen/meals_screen.dart
+++ lib/screen/meals_screen.dart
@@ -38,15 +38,42 @@ class MealsScreen extends StatelessWidget {
                   child: Container(
                     alignment: Alignment.center,
                     color: Colors.black54,
-                    padding: EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      meals[index].title,
-                      style: TextStyle(
-                        color: Colors.white,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                    child: Column(
+                      children: [
+                        Text(
+                          meals[index].title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          softWrap: true,
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: _buildIconTextWidget(
+                                Icons.monetization_on,
+                                meals[index].affordability.name,
+                              ),
+                            ),
+                            Expanded(
+                              child: _buildIconTextWidget(
+                                Icons.work,
+                                meals[index].complexity.name,
+                              ),
+                            ),
+                            Expanded(
+                              child: _buildIconTextWidget(
+                                Icons.timelapse,
+                                "${meals[index].duration} min",
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -64,6 +91,25 @@ class MealsScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  Widget _buildIconTextWidget(IconData iconData, String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(iconData, size: 16, color: Colors.white),
+        SizedBox(width: 6),
+        Flexible(
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
     );
   }
 }
